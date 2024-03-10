2090207d3e13f9c43ff88ee6e9e9ef819969b604
PSharp725 <PatrickSharp725@gmail.com>
2024-03-10 23:07:14 +0000

    Finished the create function and unit test for dbAPI.py and test_dbAPI.py with a pass

--- code/dbAPI.py
+++ code/dbAPI.py
@@ -11,12 +11,27 @@ import sqlite3
 """
 @Parm db_filename: Name of the database file to open or create
 @Return 0, Void Function, But returns 0 when working successfully
-This function will connect to the database file given to create Store, Store_Product, Product, and Category tables if they do not exist already
+This function will connect to the database file given to create Player, Scores, and ___ tables if they do not exist already
 Author: Patrick Sharp
 Last Mofdified: 3/10/2024
 """
 def create(db_filename):
     if type(db_filename) is not str:
         raise ValueError
-        
-    return None
+    conn = sqlite3.connect(db_filename)
+    c = conn.cursor()
+    
+    """
+    For the date it will be stored as an INT as Unix Time, 
+    the number of seconds since 1970-01-01 00:00:00 UTC, and use functions outlined in: 
+    https://www.sqlite.org/lang_datefunc.html to covert to a date format if needed
+    Also, see: https://www.sqlite.org/datatype3.html for more info on data types in sqlite and info on dates
+    """
+    c.execute("CREATE TABLE IF NOT EXISTS Players (playerID INT, playerName VASRCHAR(3), lastLoginDate INT );")
+    c.execute("CREATE TABLE IF NOT EXISTS Scores (playerID INT, score INT, date INT );")
+    c.execute("CREATE TABLE IF NOT EXISTS Temp ( playerID INT, playerEmail VARCHAR(45));")
+    
+    conn.commit()
+    conn.close()
+    return 0
+
--- code/tests/test_dbAPI.py
+++ code/tests/test_dbAPI.py
@@ -16,8 +16,10 @@ project_root = os.path.abspath(os.path.join(current_dir, ".."))
 sys.path.insert(0, project_root)
 import dbAPI
 
+# Global Variables
+db_filename = 'testDB'
 
-class addProductTestCase(unittest.TestCase):
+class dbAPITestCase(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
@@ -29,19 +31,21 @@ class addProductTestCase(unittest.TestCase):
 
     # Setup by making the db and tables and filling them
     def setUp(self):
-        db_filename = 'testDB'
+        global db_filename
+        dbAPI.create(db_filename)
 
     # Remove the db file
     def tearDown(self):
         try:
-            os.remove("testDB")
+            global db_filename
+            os.remove(db_filename)
         except:
             print("Error deleting test database")
             
     
     
     """
-    This test check for input errors in the database filename, starting off with ValueTypeErrors
+    This test checks for input errors in the database filename, starting off with ValueTypeErrors
     Author: Patrick Sharp
     Last Modified: 3/10/2024
     """
@@ -56,6 +60,17 @@ class addProductTestCase(unittest.TestCase):
             dbAPI.create([])
         with self.assertRaises(ValueError, msg="The database file name given is not a valid option (list)"):
             dbAPI.create(['a','b','c'])
+        
+        # Testing the table creation in the setUp database
+        global db_filename
+        conn = sqlite3.connect(db_filename)
+        c = conn.cursor()
+        c.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        tables = c.fetchall()
+            
+        assert tables[0][0] == "Players"
+        assert tables[1][0] == "Scores"
+        assert tables[2][0] == "Temp"
             
             
 if __name__ == '__main__':
